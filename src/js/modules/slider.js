a36172479cba6d4a25e6fc9aa0877b9eb8c00603
Adil Zhexenov <adilzhexenoff@gmail.com>
2023-07-27 10:49:02 +0600

    fix: change slider options

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -5,48 +5,48 @@ const slider = ({
 	sliderItemsSelector,
 	breakpoints
 }) => {
-	const sliderContainer = document.querySelector(sliderContainerSelector);
-	const sliderBtnLeft = document.querySelector(sliderBtnLeftSelector);
-	const sliderBtnRight = document.querySelector(sliderBtnRightSelector);
-	const sliderItems = document.querySelectorAll(sliderItemsSelector);
-	const sliderItemWidth = getComputedStyle(sliderItems[0]).width;
+  const sliderContainer = document.querySelector(sliderContainerSelector);
+  const sliderBtnLeft = document.querySelector(sliderBtnLeftSelector);
+  const sliderBtnRight = document.querySelector(sliderBtnRightSelector);
+  const sliderItems = document.querySelectorAll(sliderItemsSelector);
+  const sliderItemWidth = getComputedStyle(sliderItems[0]).width;
 
-	let activeIndex = 0;
+  let activeIndex = 0;
+  const screenWidth = window.innerWidth;
+  const breakpoint = breakpoints.find(({ maxWidth, minWidth }) =>
+    screenWidth <= maxWidth && screenWidth >= minWidth
+  ) || { amountToShow: 0, gap: 0 };
+  const { amountToShow, gap } = breakpoint;
 
-	const screenWidth = window.innerWidth;
-  
-	const amountToShow =
-		breakpoints.find(
-			({ maxWidth, minWidth }) =>
-				screenWidth <= maxWidth && screenWidth >= minWidth
-		)?.amountToShow || 0;
+  const updateSliderButtonsVisibility = () => {
+    sliderBtnLeft.classList.toggle('slider-btn__hide', activeIndex === 0);
+    sliderBtnRight.classList.toggle('slider-btn__hide', activeIndex === sliderItems.length - 1);
+  };
 
-	const gap =
-		breakpoints.find(
-			({ maxWidth, minWidth }) =>
-				screenWidth <= maxWidth && screenWidth >= minWidth
-		)?.gap || 0;
+  const moveSlider = () => {
+    const widthOfMove = activeIndex * parseInt(sliderItemWidth);
+    sliderContainer.style.transform = `translateX(-${activeIndex === 0 ? widthOfMove : widthOfMove + gap}px)`;
+    updateSliderButtonsVisibility();
+  };
 
-	const moveSlider = () => {
-		const widthOfMove = activeIndex * parseInt(sliderItemWidth);
-		sliderContainer.style.transform = `translateX(-${
-			activeIndex === 0 ? widthOfMove : widthOfMove + gap
-		}px)`;
-	};
+  const handleMoveLeft = () => {
+    if (activeIndex !== 0) {
+      activeIndex--;
+      moveSlider();
+    }
+  };
 
-	sliderBtnLeft.addEventListener('click', () => {
-		if (activeIndex !== 0) {
-			activeIndex--;
-			moveSlider();
-		}
-	});
+  const handleMoveRight = () => {
+    if (sliderItems.length - amountToShow !== activeIndex) {
+      activeIndex++;
+      moveSlider();
+    }
+  };
 
-	sliderBtnRight.addEventListener('click', () => {
-		if (sliderItems.length - amountToShow !== activeIndex) {
-			activeIndex++;
-			moveSlider();
-		}
-	});
+  sliderBtnLeft.addEventListener('click', handleMoveLeft);
+  sliderBtnRight.addEventListener('click', handleMoveRight);
+
+  updateSliderButtonsVisibility();
 };
 
 export default slider;
